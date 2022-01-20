89e2c7f46df0fb1e25e228dd437506e1bb972f65
chriskmil <93924532+chriskmil@users.noreply.github.com>
2022-01-19 20:31:53 -0500

    js updated

--- js/site.js
+++ js/site.js
@@ -1,12 +1,43 @@
-// display a message to the user
-function getMessage() {
-    let userMessage = document.getElementById("message").value;
-    Swal.fire(
-        'button clicked ',
-        userMessage,
-        'success'
-    );
+//Get the string from the page
+//controller function
+function getValue() {
+    document.getElementById("alert").classList.add("d-none");
+    let userString = document.getElementById("userString").value;
+    let isPalindrome = checkForPalindrome(userString);
+    displayData(isPalindrome);
+}
 
 
-    console.log(userMessage);
+//Reverse the String
+//logic function
+function checkForPalindrome(userString) {
+
+    userString = userString.toLowerCase();
+
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex, "");
+
+    let revString = "";
+    for (let index = userString.length - 1; index >= 0; index--) {
+
+        revString += userString[index];
+    }
+
+    if (revString !== userString) {
+        return false;
+    }
+    return true;
+
+}
+
+//Display the reversed string to the user
+//View Function
+function displayData(isPalindrome) {
+    if (isPalindrome == true) {
+        document.getElementById("msg").innerHTML = `Your phrase is a palindrome`;
+    } else {
+        document.getElementById("msg").innerHTML = `Your phrase is NOT palindrome`;
+    }
+    document.getElementById("alert").classList.remove("d-none");
+
 }
