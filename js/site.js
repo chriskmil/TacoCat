b663a708075fe8cb88754a31819fdaff3aff81d1
chriskmil <93924532+chriskmil@users.noreply.github.com>
2022-01-20 10:26:24 -0500

    site js fix

--- js/site.js
+++ js/site.js
@@ -33,9 +33,18 @@ function checkForPalindrome(userString) {
 //Display the reversed string to the user
 //View Function
 function displayData(isPalindrome) {
+
+    document.getElementById("alert").classList.remove("alert-success")
+    document.getElementById("alert").classList.remove("alert-danger")
+
+
     if (isPalindrome == true) {
+        document.getElementById("alert").classList.add("alert-success")
+        document.getElementById("alertHeader").innerHTML = "Well Done!"
         document.getElementById("msg").innerHTML = `Your phrase is a palindrome`;
     } else {
+        document.getElementById("alert").classList.add("alert-danger")
+        document.getElementById("alertHeader").innerHTML = "Oh No!"
         document.getElementById("msg").innerHTML = `Your phrase is NOT palindrome`;
     }
     document.getElementById("alert").classList.remove("d-none");
